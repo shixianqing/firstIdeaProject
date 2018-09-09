dbe1616f0858123ebe866872842c9e713bdec2ac
shixianqing <1434501995@qq.com>
2018-09-09 10:51:33 +0800

    发布一个java项目

---
+++ src/com/study/test/HelloWord.java
@@ -0,0 +1,13 @@
+package com.study.test;
+
+/**
+ * Created by admin on 2018/9/9.
+ */
+public class HelloWord {
+
+
+    public static void main(String[] args){
+        System.out.println("放松放松的");
+    }
+
+}
